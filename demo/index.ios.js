3f768e4fc31dc0487cc26a99910fcb0026e81dff
Hamza Sharawi <hamza.sharawi@gmail.com>
2016-05-01 13:11:12 +0300

    Add simple android demo

--- demo/index.ios.js
+++ demo/index.ios.js
@@ -1,31 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
 
+import Orientation from 'react-native-orientation';
+
 class demo extends Component {
+  constructor() {
+    super();
+    const init = Orientation.getInitialOrientation();
+    this.state = {
+      init,
+      or: init,
+    };
+    this._updateOrientation = this._updateOrientation.bind(this);
+    Orientation.addOrientationListener(this._updateOrientation);
+  }
+
+  _updateOrientation(or) {
+    this.setState({ or });
+  }
+
   render() {
+    const { init, or} = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native Orientation Demo!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          {`Initial Orientation: ${init}`}
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          {`Current Orientation: ${or}`}
         </Text>
+        <TouchableOpacity
+          onPress={Orientation.unlockAllOrientations}
+          style={styles.button}
+        >
+          <Text style={styles.instructions}>
+            Unlock All Orientations
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={Orientation.lockToPortrait}
+          style={styles.button}
+        >
+          <Text style={styles.instructions}>
+            Lock To Portrait
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={Orientation.lockToLandscape}
+          style={styles.button}
+        >
+          <Text style={styles.instructions}>
+            Lock To Landscape
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -48,6 +84,14 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  button: {
+    padding: 5,
+    margin: 5,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 3,
+    backgroundColor: 'grey',
+  }
 });
 
 AppRegistry.registerComponent('demo', () => demo);
