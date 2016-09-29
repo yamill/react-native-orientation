a2a67f36683f4f2b31dc8fce005adeaa369f7711
Ron Heft <ron@thesocialstation.com>
2016-09-29 16:07:26 -0400

    Remove unused code not apparent in rebase

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -1,23 +1,16 @@
 package com.github.yamill.orientation;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.graphics.Point;
 import android.hardware.SensorManager;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.Display;
 import android.view.OrientationEventListener;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nullable;
 
-import com.facebook.common.logging.FLog;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.LifecycleEventListener;
@@ -25,10 +18,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.common.ReactConstants;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-public class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener{
+public class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     final OrientationEventListener mOrientationEventListener;
     private Integer mOrientationValue;
     private String mOrientation;
@@ -55,17 +47,18 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
         mOrientations = isLandscapeDevice() ? ORIENTATIONS_LANDSCAPE_DEVICE : ORIENTATIONS_PORTRAIT_DEVICE;
 
         mOrientationEventListener = new OrientationEventListener(reactContext,
-             SensorManager.SENSOR_DELAY_NORMAL) {
+                SensorManager.SENSOR_DELAY_NORMAL) {
             @Override
             public void onOrientationChanged(int orientationValue) {
-                if (!mHostActive || isDeviceOrientationLocked() || !reactContext.hasActiveCatalystInstance()) return;
+                if (!mHostActive || isDeviceOrientationLocked() || !ctx.hasActiveCatalystInstance())
+                    return;
 
                 mOrientationValue = orientationValue;
 
                 if (mOrientation != null && mSpecificOrientation != null) {
                     final int halfSector = ACTIVE_SECTOR_SIZE / 2;
                     if ((orientationValue % 90) > halfSector
-                        && (orientationValue % 90) < (90 - halfSector)) {
+                            && (orientationValue % 90) < (90 - halfSector)) {
                         return;
                     }
                 }
@@ -73,8 +66,7 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
                 final String orientation = getOrientationString(orientationValue);
                 final String specificOrientation = getSpecificOrientationString(orientationValue);
 
-                final DeviceEventManagerModule.RCTDeviceEventEmitter deviceEventEmitter =
-                    (DeviceEventManagerModule.RCTDeviceEventEmitter)reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
+                final DeviceEventManagerModule.RCTDeviceEventEmitter deviceEventEmitter = ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
 
                 if (!orientation.equals(mOrientation)) {
                     mOrientation = orientation;
@@ -154,10 +146,12 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
     }
 
     @Override
-    public @Nullable Map<String, Object> getConstants() {
-      HashMap<String, Object> constants = new HashMap<String, Object>();
-      constants.put("initialOrientation", mOrientation);
-      return constants;
+    public
+    @Nullable
+    Map<String, Object> getConstants() {
+        HashMap<String, Object> constants = new HashMap<String, Object>();
+        constants.put("initialOrientation", mOrientation);
+        return constants;
     }
 
     private boolean isDeviceOrientationLocked() {
@@ -166,8 +160,8 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
             return false;
         }
         return Settings.System.getInt(
-            activity.getContentResolver(),
-            Settings.System.ACCELEROMETER_ROTATION, 0
+                activity.getContentResolver(),
+                Settings.System.ACCELEROMETER_ROTATION, 0
         ) == 0;
     }
 
@@ -185,7 +179,7 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
 
     private String getSpecificOrientationString(int orientationValue) {
         if (orientationValue < 0) return ORIENTATION_UNKNOWN;
-        final int index = (int)((float)orientationValue / 90.0 + 0.5) % 4;
+        final int index = (int) ((float) orientationValue / 90.0 + 0.5) % 4;
         return mOrientations[index];
     }
 
@@ -206,35 +200,15 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
     @Override
     public void onHostResume() {
         mHostActive = true;
-        final Activity activity = getCurrentActivity();
-
-        assert activity != null;
-        activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"));
     }
+
     @Override
     public void onHostPause() {
         mHostActive = false;
-        final Activity activity = getCurrentActivity();
-        if (activity == null) return;
-        try
-        {
-            activity.unregisterReceiver(receiver);
-        }
-        catch (java.lang.IllegalArgumentException e) {
-            FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
-        }
     }
 
     @Override
     public void onHostDestroy() {
         mHostActive = false;
-        final Activity activity = getCurrentActivity();
-        if (activity == null) return;
-        try
-        {
-            activity.unregisterReceiver(receiver);
-        }
-        catch (java.lang.IllegalArgumentException e) {
-            FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
-        }}
     }
+}
