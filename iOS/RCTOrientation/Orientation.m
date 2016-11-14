bc30579b6afee3866f0f45af341e7f7e9984683b
Pavlos Vinieratos <pvinis@gmail.com>
2016-11-14 14:29:17 +0100

    add initialSpecificOrientation

--- iOS/RCTOrientation/Orientation.m
+++ iOS/RCTOrientation/Orientation.m
@@ -178,9 +178,11 @@ RCT_EXPORT_METHOD(unlockAllOrientations)
 
   UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
   NSString *orientationStr = [self getOrientationStr:orientation];
+  NSString *specificOrientationStr = [self getSpecificOrientationStr:orientation];
 
   return @{
-    @"initialOrientation": orientationStr
+    @"initialOrientation": orientationStr,
+    @"initialSpecificOrientation": specificOrientationStr,
   };
 }
 
