bbbee22040117a569cf9f4e4c4078622d5267289
Emre Memis <emre.memis.49@gmail.com>
2020-09-30 01:49:54 +0300

    Implement getSpecificOrientation method for Android

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
 
 import com.facebook.common.logging.FLog;
@@ -25,7 +29,9 @@ import java.util.Map;
 
 import javax.annotation.Nullable;
 
-public class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener{
+public class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+    private static final int FROM_RADS_TO_DEGS = -57;
+    private static int sensorEventCount = 0;
     final BroadcastReceiver receiver;
 
     public OrientationModule(ReactApplicationContext reactContext) {
@@ -44,8 +50,8 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
                 params.putString("orientation", orientationValue);
                 if (ctx.hasActiveCatalystInstance()) {
                     ctx
-                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                    .emit("orientationDidChange", params);
+                            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                            .emit("orientationDidChange", params);
                 }
             }
         };
@@ -55,6 +61,65 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
     @Override
     public String getName() {
         return "Orientation";
+    }
+
+    @ReactMethod
+    public void getSpecificOrientation(final Callback callback) {
+        Context context = getReactApplicationContext().getApplicationContext();
+        final SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        final Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float[] values = event.values;
+                if (event.values.length > 4) {
+                    float[] truncatedRotationVector = new float[4];
+                    System.arraycopy(event.values, 0, truncatedRotationVector, 0, 4);
+                    values = truncatedRotationVector;
+                }
+                updateSpecificOrientation(callback, sensorManager, sensor, this, values);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        }, sensor, 1000 * 500);
+    }
+
+    private void updateSpecificOrientation(
+            Callback callback,
+            SensorManager sensorManager,
+            Sensor sensor,
+            SensorEventListener sensorEventListener,
+            float[] vector
+    ) {
+        int worldAxisX = SensorManager.AXIS_X;
+        int worldAxisZ = SensorManager.AXIS_Z;
+        float[] orientation = new float[3];
+        float[] rotationMatrix = new float[9];
+        float[] adjustedRotationMatrix = new float[9];
+
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, vector);
+        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
+        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
+
+        String rotation = "PORTRAIT";
+        float roll = orientation[2] * FROM_RADS_TO_DEGS;
+
+        if (roll >= 75) {
+            rotation = "LANDSCAPE-LEFT";
+        } else if (roll <= -75) {
+            rotation = "LANDSCAPE-RIGHT";
+        }
+
+        if (sensorEventCount >= 5) {
+            sensorManager.unregisterListener(sensorEventListener, sensor);
+            sensorEventCount = 0;
+            callback.invoke(null, rotation);
+        } else {
+            ++sensorEventCount;
+        }
     }
 
     @ReactMethod
@@ -116,7 +181,8 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
     }
 
     @Override
-    public @Nullable Map<String, Object> getConstants() {
+    public @Nullable
+    Map<String, Object> getConstants() {
         HashMap<String, Object> constants = new HashMap<String, Object>();
         int orientationInt = getReactApplicationContext().getResources().getConfiguration().orientation;
 
@@ -152,15 +218,14 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
         }
         activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"));
     }
+
     @Override
     public void onHostPause() {
         final Activity activity = getCurrentActivity();
         if (activity == null) return;
-        try
-        {
+        try {
             activity.unregisterReceiver(receiver);
-        }
-        catch (java.lang.IllegalArgumentException e) {
+        } catch (java.lang.IllegalArgumentException e) {
             FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
         }
     }
@@ -168,5 +233,5 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
     @Override
     public void onHostDestroy() {
 
-        }
     }
+}
